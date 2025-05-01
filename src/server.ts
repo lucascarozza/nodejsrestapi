54f972246937a215afc0fd146ba644a98e019f7e
Lucas Carozza <lucas.carozza@icloud.com>
2025-05-01 19:29:10 -0300

    Setup migrations, env vars with dotenv and Zod validation

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,31 @@
 import fastify from "fastify";
+// import { randomUUID } from "node:crypto";
 import { knex } from "./database";
+import { env } from "./env";
 
 const server = fastify();
 
-server.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
-  return tables;
+server.get("/transaction", async () => {
+  // const transaction = await knex("transactions")
+  //   .insert({
+  //     id: randomUUID(),
+  //     title: "Transação teste",
+  //     amount: 1000,
+  //   })
+  //   .returning("*");
+
+  const transaction = await knex("transactions").select("*");
+
+  // const transaction = await knex("transactions")
+  //   .where("amount", 1000)
+  //   .select("*");
+
+  return transaction;
 });
 
 server
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running.");
