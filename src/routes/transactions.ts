0c023317b5176d2f1526aa1461c3b780b4a3acf6
Lucas Carozza <lucas.carozza@icloud.com>
2025-05-02 13:47:51 -0300

    Add new transactions routes and methods

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,38 @@ import { randomUUID } from "node:crypto";
 import { knex } from "../database";
 
 export async function transactionsRoutes(server: FastifyInstance) {
+  // List Transactions
+  server.get("/", async () => {
+    const transactions = await knex("transactions").select("*");
+    return { transactions };
+  });
+
+  // Get Transaction Details
+  server.get("/:id", async (request) => {
+    // Zod Schema
+    const getTransactionDetailsParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionDetailsParamsSchema.parse(request.params);
+
+    const transaction = await knex("transactions").where("id", id).first();
+
+    return { transaction };
+  });
+
+  // Summarize Transactions
+  server.get("/summary", async () => {
+    const summary = await knex("transactions")
+      .sum("amount", {
+        as: "amount",
+      })
+      .first();
+
+    return { summary };
+  });
+
+  // Create Transaction
   server.post("/", async (request, reply) => {
     // Zod Schema
     const createTransactionBodySchema = z.object({
